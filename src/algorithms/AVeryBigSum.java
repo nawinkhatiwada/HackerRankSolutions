42841ed3d90d117b79e25a26d6525b18fb281c68
nabin.khatiwada <nabin.khatiwada@braindigit.com>
2017-05-29 15:24:33 +0545

    description added

--- src/algorithms/AVeryBigSum.java
+++ src/algorithms/AVeryBigSum.java
@@ -6,7 +6,25 @@ import java.util.Scanner;
  * Created by brainovation on 5/29/17.
  */
 public class AVeryBigSum {
+    /**
+    You are given an array of integers of size N. You need to print the sum of the elements in the array, keeping in mind that some of those integers may be quite large.
 
+    Input Format:
+    The first line of the input consists of an integer N. The next line contains N space-separated integers contained in the array.
+
+    Output Format
+    Print a single value equal to the sum of the elements in the array.
+     */
+
+    /**
+     * Sample Input:
+     * N = 5
+     *ArrayOfElement: 1000000001 1000000002 1000000003 1000000004 1000000005
+     *
+     * Output :
+     * 5000000015
+     *
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
