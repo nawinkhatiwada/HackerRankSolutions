479fb696158d525777088360fa9d1e29e7b7c223
nabin.khatiwada <nabin.khatiwada@braindigit.com>
2017-05-30 12:59:56 +0545

    description of algorithm added

--- src/algorithms/Staircase.java
+++ src/algorithms/Staircase.java
@@ -6,6 +6,20 @@ import java.util.Scanner;
  * Created by brainovation on 5/30/17.
  */
 public class Staircase {
+    /**
+     * Sample input: n=6
+     *
+     * Sample output:
+     *
+     *       #
+     *      ##
+     *     ###
+     *    ####
+     *   #####
+     *  ######
+     *
+     *
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
