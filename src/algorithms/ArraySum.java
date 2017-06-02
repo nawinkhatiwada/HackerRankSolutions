8736459052a38abfa2ce4ddca6642f81372a3415
nabin.khatiwada <nabin.khatiwada@braindigit.com>
2017-06-02 15:47:12 +0545

    MiniMaxSum class created

--- src/algorithms/ArraySum.java
+++ src/algorithms/ArraySum.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
  */
 public class ArraySum {
     public static void main(String[] args) {
-      /*
-      sample input:
-         n =  6
-         array = 1 2 3 4 10 11
-
-      sample output:
-        sum of array element
-      */
+//      /*
+//      sample input:
+//         n =  6
+//         array = 1 2 3 4 10 11
+//
+//      sample output:
+//        sum of array element
+//      */
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int sum = 0;
@@ -24,6 +24,21 @@ public class ArraySum {
             sum = sum + arr[i];
         }
         System.out.println(sum);
+
+//        Scanner scanner = new Scanner(System.in);
+//        num(scanner.nextInt());
     }
+
+
+
+//    public static void num(int num) {
+//        try {
+//            int test = 1 / num;
+//            System.out.print(num);
+//            num(num - 1);
+//        } catch (ArithmeticException e) {
+//            return;
+//        }
+//    }
 
 }
---
+++ src/algorithms/MiniMaxSum.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+/**
+ * Created by brainovation on 6/2/17.
+ */
+public class MiniMaxSum {
+    public static void main(String[] args) {
+
+    }
+}
