868f2aeb2657d87c574f89dddf5d213d2df4135f
nawinkhatiwada <nawin.khatiwada@gmail.com>
2017-06-03 11:23:08 +0545

    unwanted code removed

--- src/algorithms/MiniMaxSum.java
+++ src/algorithms/MiniMaxSum.java
@@ -6,22 +6,8 @@ import java.util.Scanner;
  * Created by brainovation on 6/2/17.
  */
 public class MiniMaxSum {
-    public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//        int[] arr = new int[5];
-//        int sum = 0;
-//        for (int i = 0; i < 5; i++) {
-//            arr[i] = in.nextInt();
-//            for (int j = 0; j < 4; j++) {
-//                if (i == j) {
-//                    j++;
-//                } else {
-//                    sum = sum + arr[j];
-//                }
-//            }
-//        }
-//        System.out.println(sum);
 
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         long max, min, sum;
         sum = max = min = in.nextLong();
