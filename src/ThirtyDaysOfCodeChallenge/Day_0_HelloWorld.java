2f250bfb50a766b2b96ebbf35b3733a5652e7918
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-10-13 16:55:52 +0545

    Hello World Challenge

---
+++ src/ThirtyDaysOfCodeChallenge/Day_0_HelloWorld.java
@@ -0,0 +1,18 @@
+package ThirtyDaysOfCodeChallenge;
+
+import java.util.Scanner;
+
+public class Day_0_HelloWorld {
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+        // Print a string literal saying "Hello, World." to stdout.
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+
+        // TODO: Write a line of code here that prints the contents of inputString to stdout.
+    }
+}
