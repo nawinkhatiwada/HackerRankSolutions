3b8fb17574606481c46e125d5429c29e7cb1470c
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-10-13 17:16:07 +0545

    Day 1 data types solved

---
+++ src/ThirtyDaysOfCodeChallenge/Day_1_DataTypes.java
@@ -0,0 +1,20 @@
+package ThirtyDaysOfCodeChallenge;
+
+import java.util.Scanner;
+
+public class Day_1_DataTypes {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        Scanner scan = new Scanner(System.in);
+        int j = scan.nextInt();
+        double e = scan.nextDouble();
+        scan.nextLine();
+        String t = scan.nextLine();
+        scan.close();
+        System.out.println(String.valueOf(i + j));
+        System.out.println(String.valueOf(d + e));
+        System.out.println(s + t);
+    }
+}
