444c35374d44cb89d7c4aa41be5911e219a9cd70
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-10-25 15:15:44 +0545

    Day 6 Lets preview solution done

--- src/thirtydaysofcodechallenge/Day_6_LetsReview.java
+++ src/thirtydaysofcodechallenge/Day_6_LetsReview.java
@@ -1,8 +1,26 @@
 package thirtydaysofcodechallenge;
 
+import java.util.Scanner;
+
 public class Day_6_LetsReview {
 
     public static void main(String[] args) {
-//        TODO
+        Scanner scanner = new Scanner(System.in);
+        int numOfInputs = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < numOfInputs; i++) {
+            String inputs = scanner.nextLine();
+            StringBuilder evenPosItem = new StringBuilder();
+            StringBuilder oddPosItem = new StringBuilder();
+
+            for (int j = 0; j < inputs.length(); j++) {
+                if (j % 2 == 0) {
+                    evenPosItem.append(inputs.charAt(j));
+                } else {
+                    oddPosItem.append(inputs.charAt(j));
+                }
+            }
+            System.out.println(evenPosItem + " " + oddPosItem);
+        }
     }
 }
